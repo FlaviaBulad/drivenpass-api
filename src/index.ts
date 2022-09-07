6df53ccb656fc0de8848e34fc2818de5a51cc793
Flávia Bulad <flavia.bulad@gmail.com>
2022-09-07 19:48:51 -0300

    config: setting up project structure and install dependencies

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { json } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import 'express-async-errors';
+
+// import router from './routes/index.js';
+// import handleError from './middlewares/errorHandler.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors(), json());
+// app.use(router);
+// app.use(handleError);
+
+const PORT: number = Number(process.env.PORT) || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
+});
